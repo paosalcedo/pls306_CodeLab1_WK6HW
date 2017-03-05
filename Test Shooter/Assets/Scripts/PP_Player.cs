0610297b36da5405d3cbfbae4e724bc39e038c27
Unknown <paosalcedo@gmail.com>
2017-03-05 13:05:28 -0500

    Added mouselook and wasd control because PS4 controller wasn't working properly

--- Test Shooter/Assets/Scripts/PP_Player.cs
+++ Test Shooter/Assets/Scripts/PP_Player.cs
@@ -12,7 +12,7 @@ public class PP_Player : MonoBehaviour {
 	public Vector2 myDirection;
 	public Vector2 myMoveAxis;
 	[SerializeField] float mySpeed = 1;
-	[SerializeField] float stickPrecision;
+	[SerializeField] float moveInertia;
 	[SerializeField] float moveSensitivity;
 
 	// Use this for initialization
@@ -24,7 +24,7 @@ public class PP_Player : MonoBehaviour {
 	{
 		UpdateMove();
 		UpdateRotation();
-		TrapperAttack();
+		UpdateAttack();
 	}
 
 	private void UpdateMove ()
@@ -43,24 +43,19 @@ public class PP_Player : MonoBehaviour {
 /********************************************************
 -----------CREATES RESISTANCE AND INERTIA--------------
 *********************************************************/
-		//Debug.Log (myMoveAxis.magnitude);
-		float t_moveAxisReduce = Time.deltaTime * stickPrecision;
-//		Debug.Log("move axis reduce: " + t_moveAxisReduce);
-//		Debug.Log("myDirection: " + myDirection.magnitude);
-//		Debug.Log("myMoveAxis: " + myMoveAxis.magnitude);
-//		Debug.Log("myRigidbody2D.velocity: " + myRigidbody2D.velocity.magnitude);
+		float t_moveAxisReduce = Time.deltaTime * moveInertia;
 
 		if (myMoveAxis.magnitude < t_moveAxisReduce) { //this is when left stick is not pressed.
 			myMoveAxis = Vector2.zero;
 		} 
 //			"UN-COMMENT" ELSE CONDITION BELOW FOR BETTER KEYBOARD SUPPORT
-//			else {		
-// 			myMoveAxis *= (myMoveAxis.magnitude - t_moveAxisReduce); 
-//			//adding Sutphin's deadzone fix
+			else {		
+ 			myMoveAxis *= (myMoveAxis.magnitude - t_moveAxisReduce); 
+			//adding Sutphin's deadzone fix
 //			myMoveAxis = myMoveAxis.normalized * 
 //						((myMoveAxis.magnitude - t_moveAxisReduce) / 
 //						(1 - t_moveAxisReduce)); 
-//		}
+		}
 
 //		float deadzone = 0.2f;		
 //		if (myMoveAxis.magnitude < deadzone)
@@ -77,48 +72,64 @@ public class PP_Player : MonoBehaviour {
 	private void UpdateRotation ()
 	{
 		
-//		ASSIGN AIMING TO THE RIGHT STICK
-			float deadzone = 0.2f;
-			float inputRightStickX = Input.GetAxis ("RightStickX");
-			float inputRightStickY = Input.GetAxis ("RightStickY");
+
+//MOUSE AIM ATTEMPT
+
+		var playerPos = Camera.main.WorldToScreenPoint(transform.position);
+	    var aimDir = Input.mousePosition - playerPos;
+		var angle = (Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg) - 90f; 
+	    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+//		Debug.Log(Mathf.Atan2(aimDir.y, aimDir.x));
+		Debug.Log(angle);
+//		float inputMouseX = Input.GetAxis("Mouse X");
+//		float inputMouseY = Input.GetAxis("Mouse Y");
+//
+//		Vector2 inputMouse = new Vector2 (inputMouseX, inputMouseY);
+//		Debug.Log(inputMouse.magnitude);
+
+//ASSIGN AIMING TO THE RIGHT STICK
+//			float deadzone = 0.2f;
+//			float inputRightStickX = Input.GetAxis ("RightStickX");
+//			float inputRightStickY = Input.GetAxis ("RightStickY");
+//			
+//			Vector2 inputRightStick;
+//			inputRightStick = new Vector2 (inputRightStickX, inputRightStickY);
 			
-			Vector2 inputRightStick;
-			inputRightStick = new Vector2 (inputRightStickX, inputRightStickY);
-			
-//Stores the last rotation angle.
+//
+//////Stores the last rotation angle.
 //			Vector3 lastRotation;
 //			lastRotation = transform.eulerAngles; 
-//			
-//			transform.eulerAngles = new Vector3 (
+//
+//				transform.eulerAngles = new Vector3 (
 //				transform.eulerAngles.x, 
 //				transform.eulerAngles.y,  
 //				Mathf.Atan2 (inputRightStick.x, inputRightStick.y) * Mathf.Rad2Deg); //binds z rotation to the right stick		
-//	
+										
 
 //Prevents snapping back to transform.eulerAngles.z = 270f when letting go of right analog stick.
-//			if (inputRightStick.magnitude < deadzone) {
+//			if (inputRightStickX + inputRightStickY < deadzone) {
 //				transform.eulerAngles = lastRotation;
 //			}		
 
 //Rigidbody version of rightstick control. 
-		
-		float lastRot = transform.eulerAngles.z; 
-		myRigidbody2D.MoveRotation (Mathf.Atan2 (inputRightStick.x, inputRightStick.y) * Mathf.Rad2Deg);
-
-		if (inputRightStick.magnitude < deadzone) {
-			myRigidbody2D.MoveRotation (lastRot);
-		}		
+//		
+//		float lastRot = transform.eulerAngles.z; 
+//		myRigidbody2D.MoveRotation (Mathf.Atan2 (inputRightStick.x, inputRightStick.y) * Mathf.Rad2Deg);
+//
+//		if (inputRightStick.magnitude < deadzone) {
+//			myRigidbody2D.MoveRotation (lastRot);
+//		}		
 	}
 
-	private void TrapperAttack ()
+	private void UpdateAttack ()
 	{
 
-		float inputAttack = Input.GetAxis ("RightTrigger");
-		if (inputAttack == 1) {
+//		float inputAttack = Input.GetButtonDown ("RightTrigger");
+		if (Input.GetButtonDown("Fire1")) {
 				trapBullet = Instantiate (Resources.Load ("Prefabs/TrapBullet") as GameObject); 
 				trapBullet.transform.position = transform.position;
 				trapBullet.transform.rotation = transform.rotation;	
-		}
+		} 
 	}
 
 }
